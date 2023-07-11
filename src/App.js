02fa4972887f6c0fefaca6e103d0afd24d2f2fde
David Pratama <davidchristianpratama@gmail.com>
2023-07-11 17:31:07 +0700

    success to making jest testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ const [heading, setHeading] = useState("Logged Out");
 
+ const onClick= () => {
+  setHeading("Logged In");
+ };
+
+ return (
+  <>
+
+   <button type="button" onClick={onClick}>
+    Log In
+   </button>
+   <h1>{heading}</h1>
+  </>
+ );
+};
+
+export const Title = () => {
+ return (
+  <h1>Hello</h1>
+ )}
 export default App;
